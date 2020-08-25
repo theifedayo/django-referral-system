6ed29c459a5d5fc07f4bde906276e15091e6a55d
ifedayo <adesiyanifedayo@gmail.com>
2020-08-25 02:12:43 +0100

    Add signup,login,ref_id urls

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from .views import (
     ItemDetailView,
     CheckoutView,
-    HomeView,
+    # HomeView,
     OrderSummaryView,
     add_to_cart,
     remove_from_cart,
     remove_single_item_from_cart,
-    # PaymentView,
+    PaymentView,
     AddCouponView,
+    ProfileDetailView,
     # RequestRefundView
 )
 from django.contrib.auth.views import LoginView
@@ -17,7 +18,7 @@ from core import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    # path('', index.views, name='home'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
@@ -26,8 +27,13 @@ urlpatterns = [
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
     path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
          name='remove-single-item-from-cart'),
-    # path('signup/', views.signup, name='signup'),
-    # path('login/', LoginView.as_view(), name='login'),
-    # path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('sellers/', views.sellers_signup, name='sellers-signup'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.search_lunnex, name='search-lunnex'),
+    path('search/', views.results, name='results'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('payment/stripe/', PaymentView.as_view(), name='payment'),
+    path('payment/paystack/', views.paystack, name='payment'),
     # path('request-refund/', RequestRefundView.as_view(), name='request-refund')
 ]
