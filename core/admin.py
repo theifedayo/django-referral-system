bf6790745253e85397d85bd164e4466e45ac58eb
ifedayo <adesiyanifedayo@gmail.com>
2020-08-25 02:11:13 +0100

    Add referrals models

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address, UserProfile
+from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address, UserProfile, Profile, Referral
 
 
 def make_refund_accepted(modeladmin, request, queryset):
@@ -63,3 +63,5 @@ admin.site.register(Coupon)
 admin.site.register(Refund)
 admin.site.register(Address, AddressAdmin)
 admin.site.register(UserProfile)
+admin.site.register(Profile)
+admin.site.register(Referral)
