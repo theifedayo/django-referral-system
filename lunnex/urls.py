7fe6ae8ad56c1e358ad546372c493043a77f86ac
ifedayo <adesiyanifedayo@gmail.com>
2020-08-28 20:20:05 +0100

    Referral changes

--- lunnex/urls.py
+++ lunnex/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path("ref/", include("django_reflinks.urls")),
     path('', include('core.urls', namespace='core'))
 ]
