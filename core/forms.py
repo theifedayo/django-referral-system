493ce58e80bb8f5ef20683095c56d88259631bbb
Adedamola <adedadamola@gmail.com>
2020-08-31 21:11:38 +0100

    Initial commit

--- core/forms.py
+++ core/forms.py
@@ -5,21 +5,10 @@ from django_countries.widgets import CountrySelectWidget
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from allauth.account.forms import SignupForm 
-from .models import Profile, Referral, Item
+from .models import Profile, Referral
 
 
 
-# PAYMENT_CHOICES = (
-#     ('S', 'Stripe'),
-#     ('P', 'PayPal'),
-#     ('PS', 'PayStack')
-# )
-
-class ItemForm(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ['title','price','discount_price','paystack_link',
-            'category','label','slug','description','image']
 
 class ReferralForm(forms.ModelForm):
     class Meta:
@@ -32,54 +21,4 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ['store_name']
 
-class CheckoutForm(forms.Form):
-    shipping_address = forms.CharField(required=False)
-    shipping_address2 = forms.CharField(required=False)
-    shipping_country = CountryField(blank_label='(select country)').formfield(
-        required=False,
-        widget=CountrySelectWidget(attrs={
-            'class': 'custom-select d-block w-100',
-        }))
-    shipping_zip = forms.CharField(required=False)
-
-    billing_address = forms.CharField(required=False)
-    billing_address2 = forms.CharField(required=False)
-    billing_country = CountryField(blank_label='(select country)').formfield(
-        required=False,
-        widget=CountrySelectWidget(attrs={
-            'class': 'custom-select d-block w-100',
-        }))
-    billing_zip = forms.CharField(required=False)
-
-    same_billing_address = forms.BooleanField(required=False)
-    set_default_shipping = forms.BooleanField(required=False)
-    use_default_shipping = forms.BooleanField(required=False)
-    set_default_billing = forms.BooleanField(required=False)
-    use_default_billing = forms.BooleanField(required=False)
-
-    # payment_option = forms.ChoiceField(
-    #     widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
-
-
-class CouponForm(forms.Form):
-    code = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Promo code',
-        'aria-label': 'Recipient\'s username',
-        'aria-describedby': 'basic-addon2'
-    }))
-
-
-class RefundForm(forms.Form):
-    ref_code = forms.CharField()
-    message = forms.CharField(widget=forms.Textarea(attrs={
-        'rows': 4
-    }))
-    email = forms.EmailField()
-
-
-class PaymentForm(forms.Form):
-    stripeToken = forms.CharField(required=False)
-    save = forms.BooleanField(required=False)
-    use_default = forms.BooleanField(required=False)
 
--- core/urls.py
+++ core/urls.py
@@ -1,44 +1,14 @@
 from django.urls import path
-from .views import (
-    ItemDetailView,
-    CheckoutView,
-    # HomeView,
-    OrderSummaryView,
-    add_to_cart,
-    remove_from_cart,
-    remove_single_item_from_cart,
-    PaymentView,
-    AddCouponView,
-    ProfileDetailView,
-    # RequestRefundView
-)
+from .views import ProfileDetailView  
 from django.contrib.auth.views import LoginView
 from core import views
 
 app_name = 'core'
 
 urlpatterns = [
-    # path('', index.views, name='home'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
-    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
-         name='remove-single-item-from-cart'),
+
     path('sellers/', views.sellers_signup, name='sellers-signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/create-prod', views.item_create_view, name='item-create-view'),
-    path('', views.search_lunnex, name='search-lunnex'),
-    path('search/', views.results, name='results'),
     path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(), name='login'),
-    path('payment-options/', views.payment_option, name='payment_option'),
-    path('payment/stripe/', PaymentView.as_view(), name='payment'),
-    path('payment/paystack/', views.paystack, name='payment-paystack'),
-    path('process/', views.payment_process, name="process"),
-    path('done/', views.payment_done, name="done"),
-    path('canceled', views.payment_canceled, name='canceled')
-    # path('request-refund/', RequestRefundView.as_view(), name='request-refund')
 ]
